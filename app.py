7396ba8ea45a6306bf6cc5109cf86258781a4183
ha5804 <zixpotf5803@gmail.com>
2025-07-17 15:05:02 +0900

    make button and write tempete

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Flask!"
+    return render_template('home.html')
+
+@app.route('/lstm/')
+def learn():
+    return render_template('lstm/index.html')
+
+@app.route('/lstm/step1')
+def step1():
+    return render_template('lstm/step1.html')
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
