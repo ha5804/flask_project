8474f031ce6a1105ba04c709afd8174de1295cf8
ha5804 <zixpotf5803@gmail.com>
2025-07-24 14:29:46 +0900

    input button activation

--- app.py
+++ app.py
@@ -6,21 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/lstm/')
+@app.route('/balance/')
 def learn():
     return render_template('lstm/index.html')
-
-@app.route('/lstm/step0')
-def step0():
-    return render_template('lstm/step0.html')
-
-@app.route('/lstm/step1')
-def step1():
-    return render_template('lstm/step1.html')
-
-@app.route('/lstm/step2')
-def step2():
-    return render_template('lstm/step2.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
