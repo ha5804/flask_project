2b6ed85f18ed85952ba15dd042606f7fc092b1ae
ha5804 <zixpotf5803@gmail.com>
2025-07-24 14:51:15 +0900

    make step1

--- app.py
+++ app.py
@@ -11,8 +11,11 @@ def home():
 def input_balance():
     balance = request.form["balance"]
     # balance는 문자열
-    return render_template("step1.html", balance = balance) 
+    return render_template("index.html", balance = balance) 
     # reder_template으로 홈페이지를 연결하고, balance 값을 전달한다.
 
+@app.route('/step1/')
+def step1():
+    return render_template("step1.html")
 if __name__ == '__main__':
     app.run(debug=True)
