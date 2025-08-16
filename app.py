2318c9f5b3637d741ae794ec2560e175ca8700a9
ha5804 <zixpotf5803@gmail.com>
2025-08-16 13:08:43 +0900

    change title

--- app.py
+++ app.py
@@ -59,6 +59,7 @@ def logout():
 def step1():
     return render_template("/step1.html")
 
-if __name__ == '__main__':
-    app.run(debug = True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
 
